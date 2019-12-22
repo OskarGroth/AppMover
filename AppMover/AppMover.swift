f47fd1cf4a499c0b086a54f97d40b9de93ec3cc0
Oskar Groth <oskar@cindori.org>
2019-12-22 01:27:04 +0100

    Cleanup

--- AppMover/AppMover.swift
+++ AppMover/AppMover.swift
@@ -9,54 +9,6 @@
 import AppKit
 import Security
 
-extension URL {
-    
-    var representsBundle: Bool {
-        pathExtension == "app"
-    }
-    
-    var isValid: Bool {
-        !path.trimmingCharacters(in: .whitespaces).isEmpty
-    }
-    
-    var numberOfFilesInDirectory: Int {
-        (try? FileManager.default.contentsOfDirectory(atPath: path))?.count ?? 0
-    }
-    
-}
-
-extension Bundle {
-    
-    var localizedName: String {
-        NSRunningApplication.current.localizedName ?? "The App"
-    }
-    
-    var isInstalled: Bool {
-        NSSearchPathForDirectoriesInDomains(.applicationDirectory, .allDomainsMask, true).contains(where: { $0.hasPrefix(bundlePath)
-        }) || bundlePath.split(separator: "/").contains("Applications")
-    }
-    
-    func copy(to url: URL) throws {
-        try FileManager.default.copyItem(at: bundleURL, to: url)
-    }
-    
-}
-
-extension Process {
-    
-    static func runTask(command: String, arguments: [String] = [], completion: ((Int32) -> Void)? = nil) {
-        let task = Process()
-        task.launchPath = command
-        task.arguments = arguments
-        task.terminationHandler = { task in
-            completion?(task.terminationStatus)
-        }
-        task.launch()
-    }
-    
-}
-
-
 public enum AppMover {
         
     public static func moveIfNecessary() {
---
+++ AppMover/Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  Extensions.swift
+//  AppMover
+//
+//  Created by Oskar Groth on 2019-12-22.
+//  Copyright © 2019 Oskar Groth. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    
+    var representsBundle: Bool {
+        pathExtension == "app"
+    }
+    
+    var isValid: Bool {
+        !path.trimmingCharacters(in: .whitespaces).isEmpty
+    }
+    
+    var numberOfFilesInDirectory: Int {
+        (try? FileManager.default.contentsOfDirectory(atPath: path))?.count ?? 0
+    }
+    
+}
+
+extension Bundle {
+    
+    var localizedName: String {
+        NSRunningApplication.current.localizedName ?? "The App"
+    }
+    
+    var isInstalled: Bool {
+        NSSearchPathForDirectoriesInDomains(.applicationDirectory, .allDomainsMask, true).contains(where: { $0.hasPrefix(bundlePath)
+        }) || bundlePath.split(separator: "/").contains("Applications")
+    }
+    
+    func copy(to url: URL) throws {
+        try FileManager.default.copyItem(at: bundleURL, to: url)
+    }
+    
+}
+
+extension Process {
+    
+    static func runTask(command: String, arguments: [String] = [], completion: ((Int32) -> Void)? = nil) {
+        let task = Process()
+        task.launchPath = command
+        task.arguments = arguments
+        task.terminationHandler = { task in
+            completion?(task.terminationStatus)
+        }
+        task.launch()
+    }
+    
+}
